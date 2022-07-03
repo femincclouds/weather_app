505cfda7088b9d9c37c6a61bd99d3098eb41933a
femincclouds <femin.justin@cognitiveclouds.com>
2022-07-03 12:32:29 +0530

    state management using zustand

---
+++ src/state/Data.ts
@@ -0,0 +1,49 @@
+import create from "zustand";
+import { emptyFunction } from "../utils/common";
+import { createSelectors } from "./state";
+
+const initialState: IAppState = {
+  countryDetails: {
+    capital: "",
+    population: undefined,
+    latlng: [],
+    flag: undefined,
+  },
+  weatherDetails: {
+    temperature: undefined,
+    waether_icons: "",
+    wind_speed: undefined,
+    precip: undefined,
+  },
+  setCountryDetails: emptyFunction,
+  setWeatherDetails: emptyFunction,
+};
+
+export const useAppState = create<IAppState>((set, get) => ({
+  ...initialState,
+  setCountryDetails: (countryDetails) => set({ countryDetails }),
+  setWeatherDetails: (weatherDetails) => set({ weatherDetails }),
+}));
+
+export const appStateSelectors = createSelectors(initialState);
+
+export interface IAppState {
+  countryDetails: ICountryDetails;
+  weatherDetails: IWeatherDetails;
+  setCountryDetails: (country: ICountryDetails) => void;
+  setWeatherDetails: (weather: IWeatherDetails) => void;
+}
+
+export interface ICountryDetails {
+  capital: string;
+  population?: number;
+  latlng: number[];
+  flag: any;
+}
+
+export interface IWeatherDetails {
+  temperature?: number;
+  waether_icons: any;
+  wind_speed?: number;
+  precip?: number;
+}
